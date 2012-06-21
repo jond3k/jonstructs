c9de8f14a4b55a1dde1d5c46e552266541bc6022
Jon Davey <jond3k@gmail.com>
2012-06-21 22:52:45 +0100

    resolved package names

--- src/main/scala/com/github/jond3k/jonstructs/tasks/ThreadLoopHandle.scala
+++ src/main/scala/com/github/jond3k/jonstructs/tasks/ThreadLoopHandle.scala
@@ -1,7 +1,7 @@
 package com.github.jond3k.jonstructs.tasks
 
-import com.mediasift.push.util.{Logging, Terminable}
 import java.util.concurrent.{Future, TimeUnit, Executors}
+import com.github.jond3k.jonstructs.{Logging, Terminable}
 
 class ThreadLoopHandle(action: () => Unit, sleepMs: Long, deferred: Boolean)
   extends Terminable
